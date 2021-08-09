9c761499facf5d71cca80cb8dd021c7b2687274d
Josoe Ichuta <43251070+Jhoshoa@users.noreply.github.com>
2021-08-09 01:17:54 -0400

    Add project files.

---
+++ CreatePdf/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+
+namespace CreatePdf
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Main/Css").Include("~/Content/Css/main.css"));
+        }
+    }
+}
---
+++ CreatePdf/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CreatePdf.MvcApplication" Language="C#" %>
